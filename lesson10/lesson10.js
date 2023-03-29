31b551d3b7ce10e4425c251ba9631d2830abc4fa
DoHoangLuan <dohoangluan26x8@gmail.com>
2023-03-29 19:15:25 +0700

    lesson 10

---
+++ lesson10/lesson10.js
@@ -0,0 +1,56 @@
+// let input = NaN;
+
+// do {
+//     input = Number(prompt("Nhập vào số: "));
+// } while (input == NaN)
+
+
+// function inputNumber(label){
+//     let input = NaN;
+
+// do {
+//     input = Number(prompt("Nhập vào số:"" + label + ":""));
+// } while (isNaN(input));
+// return input
+// }
+//  a = inputNumber();
+//  b = inputNumber();
+
+
+
+
+// function High () {
+//     alert ("Hello World")
+// }
+// High();
+
+
+
+// let a = 100;
+// let b = 10;
+
+// function test (){
+//     function calculateAB(){
+//         return a + b;
+//     }
+//     return calculateAB();
+// }
+// console.log(test());
+
+// function doSomething () {
+//     console.log("Do something here");
+// }
+// function executeAfterSeconds(second, work){
+//     console.log("Thực thi gì đó sau " + second + "giây");
+//     setTimeout(work,second);
+// }
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = [];
+
+
+function IsNghiem (){
+        
+}
